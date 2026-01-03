1f0ad058b540818069060c0e3a7ffc27d73d7fad
lwmacct <lwmacct@icloud.com>
2026-01-03 08:16:43 +0800

    chore 2026-01-03 08:16:42 CST

--- pkg/ctxutil/ctxutil.go
+++ pkg/ctxutil/ctxutil.go
@@ -5,29 +5,20 @@ import (
 )
 
 const (
-	// Permissions 表示上下文中存储权限列表的键
 	Permissions = "permissions"
-
-	// Roles 表示上下文中存储角色列表的键
-	Roles = "roles"
-
-	// Username 表示上下文中存储用户名的键
-	Username = "username"
-
-	// UserID 表示上下文中存储用户 ID 的键
-	UserID = "user_id"
-
-	// UserRole 表示上下文中存储用户角色的键
-	UserRole = "user_role"
-
-	// OrgID 表示上下文中存储组织 ID 的键
-	OrgID = "org_id"
-
-	// TeamID 表示上下文中存储团队 ID 的键
-	TeamID = "team_id"
-
-	// RequestID 表示上下文中存储请求 ID 的键
-	RequestID = "request_id"
+	OperationID = "operation"
+	Roles       = "roles"
+	Username    = "username"
+	UserID      = "user_id"
+	UserRole    = "user_role"
+	OrgID       = "org_id"
+	TeamID      = "team_id"
+	RequestID   = "request_id"
+	Email       = "email"
+	AuthType    = "auth_type"
+	IsAdmin     = "is_admin"
+	Locale      = "locale"
+	Timezone    = "timezone"
 )
 
 // Get 从 Context 安全获取指定 key 的值并断言为类型 T。
